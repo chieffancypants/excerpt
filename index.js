b8ddcc766aa29c25379cc72e3489eed16de2508f
chieffancypants <chieffancypants@gmail.com>
2013-07-16 17:11:00 -0400

    Excerpt can now be run in the browser as well

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 
-exports = module.exports = excerpt;
+(function() {
 
-function excerpt(text, phrase, radius, ending) {
-  var append, prepend, phraseLen, pos, startPos, endPos, excerpt, textLen;
-  if (!text || !phrase) {
-    return text;
+  // Allow this to be used with node or the browser:
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = excerpt;
+    }
+    exports.excerpt = excerpt;
+  } else {
+    this.excerpt = excerpt;
   }
 
-  radius = radius || '100';
-  ending = ending || '...';
+  function excerpt(text, phrase, radius, ending) {
+    var append, prepend, phraseLen, pos, startPos, endPos, excerptText, textLen;
+    if (!text || !phrase) {
+      return text;
+    }
 
-  append = prepend = ending;
-  phraseLen = phrase.length;
-  textLen = text.length;
+    radius = radius || '100';
+    ending = ending || '...';
 
-  pos = text.toLowerCase().indexOf(phrase.toLowerCase());
-  if (pos === false) {
-    return text.substr(0, radius) + ending;
+    append = prepend = ending;
+    phraseLen = phrase.length;
+    textLen = text.length;
+
+    pos = text.toLowerCase().indexOf(phrase.toLowerCase());
+    if (pos === false) {
+      return text.substr(0, radius) + ending;
+    }
+
+    startPos = pos - radius;
+    if (startPos <= 0) {
+      startPos = 0;
+      prepend = '';
+    }
+
+    endPos = pos + phraseLen + radius;
+    if (endPos >= textLen) {
+      endPos = textLen;
+      append = '';
+    }
+
+    excerptText = text.substr(startPos, endPos - startPos);
+    excerptText = prepend + excerptText + append;
+
+    return excerptText;
   }
 
-  startPos = pos - radius;
-  if (startPos <= 0) {
-    startPos = 0;
-    prepend = '';
-  }
 
-  endPos = pos + phraseLen + radius;
-  if (endPos >= textLen) {
-    endPos = textLen;
-    append = '';
-  }
-
-  excerpt = text.substr(startPos, endPos - startPos);
-  excerpt = prepend + excerpt + append;
-
-  return excerpt;
-}
+}).call(this);
